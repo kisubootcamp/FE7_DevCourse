a891fa32e16757e7a8939d404c2a2dd841c25578
DESKTOP-LRRE74L\JIHO <kjh04215@gmail.com>
2025-08-05 18:04:34 +0900

    20250805 학습파일

---
+++ learn-js/01/01.js
@@ -0,0 +1,22 @@
+// 1
+const x = 10;
+const y = 20;
+const sum = x + y;
+console.log(sum);
+
+// 2
+let name = "alice";
+name = "Bob";
+console.log(name);
+
+// 3
+const PI = 3.14;
+console.log(PI);
+
+//4
+let a = 5;
+let b = 10;
+const temp = a;
+a = b;
+b = temp;
+console.log(a, b);
