8bf4002ccdea93129de8142b7661ac994b209196
suyeon <suyeon1104@gmail.com>
2025-08-05 17:34:05 +0900

    '20250805-js'

---
+++ learn-js/01/02-ex.js
@@ -0,0 +1,25 @@
+// 1. 변수 선언과 할당
+const x = 10;
+const y = 20;
+const sum = x + y;
+console.log(sum);
+
+// 2. 변수 재할당
+let name = "Alice";
+name ="Bob";
+console.log(name);
+
+// 3. 상수
+const PI = 3.14;
+console.log(PI);
+
+// 4. 변수 스왑
+let a = 5;
+let b = 10;
+let tmp;
+
+tmp = a;
+a = b;
+b = tmp;
+
+console.log(a, b);
