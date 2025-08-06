81a76fec04e11b5dcd24dce5c92dc938ffd028d5
Sage <prestoferoce@gmail.com>
2025-08-06 16:05:12 +0900

    feat: JS 기초 자료형 및 연산자

---
+++ learn-js/02/01.js
@@ -0,0 +1,31 @@
+/*
+
+자료형
+
+# 원시형
+Number
+String
+Boolean
+Symbol
+undefined
+null
+BigInt(ES11)
+
+# 참조형
+Array
+
+Object {}
+key + value = property
+
+Function
+
+---
+
+파스칼 케이스 - 모든 단어의 첫글자가 대문자, UserName
+카멜 케이스 - 연결된 단어의 첫글자는 대문자, userName
+스네이크 케이스 - 단어를 연결할 때는_, user_name
+케밥 케이스 - 단어를 연결할 때 -, user-name
+
+typeof -> 타입 확인
+
+*/
