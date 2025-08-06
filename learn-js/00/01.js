817cc9ee27e3b72064554468f598edc57d348a5a
ivoryIvoryShield <garlatonic@kakao.com>
2025-08-06 14:08:14 +0900

    250806- install node package

---
+++ learn-js/00/01.js
@@ -0,0 +1,7 @@
+// npm init npm 초기화 -> 해당 디렉토리에서 package.json 파일 생성
+// npm install eslint --save-dev
+// node_modules 폴더 생성
+
+
+// https://developer88.tistory.com/entry/NPM-%EC%9D%98-packagejson%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%9C-%ED%9A%A8%EC%9C%A8%EC%A0%81-%EC%84%A4%EC%B9%98-%EB%B0%A9%EB%B2%95-NodeJS
+// 이거 보고 다시 쳌하면 될듯
