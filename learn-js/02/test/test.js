ceb9e7b0142a9c129a961ecb7e735135b8cdf6e2
SeHyun <rla6170@naver.com>
2025-08-06 16:35:20 +0900

    20250806 학습 내용 추가

---
+++ learn-js/02/test/test.js
@@ -0,0 +1,17 @@
+// 문제 1
+let num = 42; // number
+let name = "John"; // string
+let isTrue = true; // boolean
+let person = { name: "Alice", age: 30 }; // object
+let fruits = ["apple", "banana", "orange"]; // object
+
+console.log(num, name, isTrue, person, fruits);
+
+// 문제 2
+let length = 16; // number
+let lastName = "Brown"; // string
+let score = [10, 20]; // object
+let x = {
+    firstName: "Nick",
+    lastName: "Doe"
+}; // object
