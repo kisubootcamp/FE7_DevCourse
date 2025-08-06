9810b046847160f20f9e6a67fac636f57ba8fd19
peachPetal <toneba3296@gmail.com>
2025-08-06 16:08:40 +0900

    2025_08_06

--- Learn_JS/01/01ex.js
+++ Learn_JS/01/01ex.js
@@ -1,12 +1,12 @@
 // 1. 변수 선언과 할당
-const x = 10; 
+const x = 10;
 const y = 20;
 const sum = x + y;
 console.log(sum);
 
 // 2. 변수 재할당
-let name = 'Alice';
-name = 'Bob';
+let name = "Alice";
+name = "Bob";
 console.log(name);
 
 // 3. 상수
@@ -14,7 +14,7 @@ const PI = 3.14;
 console.log(PI);
 
 // 4. 변수 스왑
-let a = 5; 
+let a = 5;
 let b = 10;
 // const temp = a;
 // a = b;
@@ -22,5 +22,3 @@ let b = 10;
 
 [a, b] = [b, a];
 console.log(a, b);
-
-
