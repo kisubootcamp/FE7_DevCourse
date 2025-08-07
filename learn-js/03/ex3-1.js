9bb0e68d42e73a197d5b6c0e0d97f7b8c2ca535b
ivoryIvoryShield <garlatonic@kakao.com>
2025-08-07 23:26:29 +0900

    250807 - 4번 다시 풂

--- learn-js/03/ex3-1.js
+++ learn-js/03/ex3-1.js
@@ -5,9 +5,9 @@ let n = 10; // 원하는 항의 수
 let cnt = 0;
 let answer = [];
 do {
-  if (cnt === 0) answer.push(0);
-  if (cnt === 1) answer.push(1);
-  if (cnt > 1) answer.push(answer[cnt - 2] + answer[cnt - 1]);
-  cnt++;
+	if (cnt === 0) answer.push(0);
+	if (cnt === 1) answer.push(1);
+	if (cnt > 1) answer.push(answer[cnt - 2] + answer[cnt - 1]);
+	cnt++;
 } while (cnt < n);
 console.log(`피보나치 수열(${n}항):`, answer);
