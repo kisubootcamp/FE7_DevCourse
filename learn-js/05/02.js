1b1964297e0b04559dcaa4baab7b88f9a98f9867
DESKTOP-LRRE74L\JIHO <kjh04215@gmail.com>
2025-08-11 16:02:26 +0900

    0811

---
+++ learn-js/05/02.js
@@ -0,0 +1,39 @@
+// 함수의 호이스팅
+
+greet();
+
+function greet() {
+  console.log("Hello");
+}
+
+/*
+함수의 본문 전체가 호이스팅 됨
+*/
+
+//////
+
+greet2();
+
+var greet2 = function greet2() {
+  console.log("Hello2");
+};
+//TypeError: greet2 is not a function
+/*
+변수 greet2의 선언부만 호이스팅된다. 함수는 할당부
+*/
+
+///
+
+var num;
+function num() {
+  return 10;
+}
+console.log(num);
+
+num = 5;
+console.log(num);
+console.log(num()); // 에러
+/*
+num = 5에 의해서 num은 함수가 아니라 5가 되었음.
+따라서 num is not a function 에러가 발생
+*/
