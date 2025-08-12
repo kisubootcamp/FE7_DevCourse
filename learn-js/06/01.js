44a26c9ba36192769e124218662a642ae533f7c2
suyeon <suyeon1104@gmail.com>
2025-08-12 20:59:38 +0900

    20250812

---
+++ learn-js/06/01.js
@@ -0,0 +1,17 @@
+// 호이스팅
+// 호이스팅은 선언과 할당 중에서 선언을 최상위로 끌어올리는 행위
+
+console.log(num); // error
+let num = 10;
+console.log(num);
+
+printName();
+// 함수 선언문
+function printName() {
+    console.log("greet");
+}
+
+// 함수 표현식
+const printName = () => {
+    console.log("greet");
+}
