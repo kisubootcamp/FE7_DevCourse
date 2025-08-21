91bdcc79d508e65f07c18ca0eabe603abf4cfe65
변수연 <varyeon@byeonsuyeon-ui-MacBookAir.local>
2025-08-21 22:53:26 +0900

    20250821

--- learn-js/10/ex1.js
+++ learn-js/10/ex1.js
@@ -1,4 +1,5 @@
 // 연습 문제
+// 다시 해보기
 
 // 1. 기본 클래스 생성
 class Car {
@@ -61,3 +62,113 @@ class Rectangle{
 
 const rect = new Rectangle(5, 10);
 console.log(rect.area); // 50
+
+// 5
+class Counter {
+  static count = 0;
+  static increment() {
+    this.count++;
+  }
+  static getValue() {
+    return this.count;
+  }
+}
+Counter.increment();
+Counter.increment();
+console.log(Counter.getValue());
+
+// 6
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  // 구식 방법
+  // shorthand method
+  greet() {
+    return `${this.name} 안녕!`;
+  }
+}
+
+const person = new Person("이순신");
+console.log(person.greet()); // 이순신 안녕!
+
+// 7
+class Shape {
+  area() {
+    // throw new Error("메서드를 구현해야 합니다.");
+    return 0;
+  }
+}
+
+class Circle extends Shape {
+  constructor(radius) {
+    super();
+    this.radius = radius;
+  }
+  area() {
+    return 3.15 * this.radius * this.radius;
+  }
+}
+
+class Rect extends Shape {
+  constructor(value) {
+    super();
+    this.width = value;
+    this.height = value;
+  }
+  area() {
+    return this.width * this.height;
+  }
+}
+
+const circle = new Circle(5);
+console.log(circle.area()); // 5 * 5 * 3.14
+
+const rects = new Rect(5);
+console.log(rects.area()); // 25 (5*5)
+
+// 8
+class Builder {
+  constructor() {
+    this.value = "";
+  }
+  append(text) {
+    this.value += text;
+    return this;
+  }
+  getValue() {
+    return this.value;
+  }
+}
+const builder = new Builder();
+const result = builder.append("Hello, ").append("World!").getValue();
+console.log(result); // Hello, World!
+
+// 9
+class Book {
+  static count = 0;
+  constructor(title) {
+    this.title = title;
+    Book.count++;
+  }
+
+  static getCount() {
+    return Book.count;
+  }
+}
+const book1 = new Book("1984");
+const book2 = new Book("하이퍼리얼리티");
+console.log(Book.getCount()); // 2
+
+// 10
+class User {
+  constructor(name, age = 18) {
+    this.name = name;
+    this.age = age;
+  }
+  info() {
+    return `${this.name}의 나이는 ${this.age}세입니다.`;
+  }
+}
+const user = new User("홍길동");
+console.log(user.info()); // 홍길동의 나이는 18세입니다.
--- learn-js/11/ex1.js
+++ learn-js/11/ex1.js
@@ -7,6 +7,17 @@ const obj2 = { b: 3, c: 4 };
 const mergeObjects1 = Object.assign({},obj1, obj2);
 console.log(mergeObjects1); // { a: 1, b: 3, c: 4 }
 
+///
+// 작동이 안 됨 : 다시
+/*
+function mergeObjects(obj1, obj2){
+return ...obj1, ...obj2;
+}
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 3, c: 4 };
+console.log(mergeObjects(obj1, obj2)); // { a: 1, b: 3, c: 4 }
+*/
+
 const obj3 = { x: 10 };
 const obj4 = { y: 20 };
 const mergeObjects2 = Object.assign({},obj3, obj4);
@@ -45,11 +56,22 @@ console.log(objectToArray4); // []
 
 // delete user.age;
 const obj13 = { a: 1, b: 2, c: 3 };
-//const keyToDelete = 'b';
-//deleteKey(obj13,keyToDelete) = delete obj13.keyToDelete;
-//console.log(deleteKey(obj13, keyToDelete)); // { a: 1, c: 3 }
 delete obj13.b;
 console.log(obj13);
+
+/// 깊은 복사
+// 작동이 안 됨 : 다시
+/*
+function deleteKey(obj13,key){
+  const copy = structuredClone(obj13);
+  delete copy[key];
+  return copy;
+}
+const obj13 = { a: 1, b: 2, c: 3 };
+const keyToDelete = 'b';
+deleteKey(obj13,keyToDelete) = delete obj13.keyToDelete;
+console.log(deleteKey(obj13, keyToDelete)); // { a: 1, c: 3 }
+*/
 
 const obj14 = { x: 10, y: 20, z: 30 };
 //const keyToDelete2 = 'y';
@@ -71,7 +93,15 @@ console.log(obj16);
 
 ////////////////////////////////////////////////////
 
-// 4.
+// 4. *
+
+///
+function findKeyByValue(obj17, value) {
+  for (const k in obj17){
+    if (obj17[k]===value) return k;
+  }
+  return null;
+}
 
 const obj17 = { a: 1, b: 2, c: 3 };
 console.log(findKeyByValue(obj17, 2)); // 'b'
@@ -80,6 +110,18 @@ console.log(findKeyByValue(obj17, 1)); // 'a'
 console.log(findKeyByValue({}, 1)); // null
 
 // 5.
+
+function multiplyValues(obj){
+  let result = 1;
+  for (const k in obj){
+    if (Object.hasOwn(obj,k)) {
+      result *= obj[k];
+    }
+  }
+  return result;
+}
+// if문 없어도 작동, 안전장치
+
 const obj18 = { a: 1, b: 2, c: 3 };
 console.log(multiplyValues(obj)); // 6
 
@@ -91,3 +133,64 @@ console.log(multiplyValues(obj3)); // 0
 
 const obj21 = {};
 console.log(multiplyValues(obj4)); // 1 (곱셈의 항등원)
+
+// 6
+
+const hasOwnKey = (obj,key) => Object.hasOwn(obj,key);
+
+const obj = Object.create({ inherited : 1});
+obj.own = 2;
+
+console.log(hasOwnKey(obj, "own"));        // true
+console.log(hasOwnKey(obj, "inherited"));  // false
+console.log(hasOwnKey({}, "a"));           // false
+console.log(hasOwnKey({ a: undefined }, "a")); // true
+
+// 7.
+
+function pick(obj, keys){
+  const out = {};
+  for (const key of keys) {
+    if (Object.hasOwn(obj, key)){
+      out[key] = obj[key];
+    }
+  }
+  return out;
+}
+
+console.log(pick({ a: 1, b: 2, c: 3 }, ["a", "c"])); // { a: 1, c: 3 }
+console.log(pick({ x: 10, y: 20 }, ["z"]));          // {}
+console.log(pick({}, ["a"]));                        // {}
+console.log(pick({ a: 1 }, []));                     // {}
+
+// 8.
+
+function omit(obj, keys){
+  const result = {};
+
+  for (const key in obj){
+    if (Object.hasOwn(obj, key)){
+   let should = false; // 현재 key를 제외해야 할지 표시하는 플래그
+    for (let i =0; i <keys.length; i++){
+      if (keys[i]=key){
+        should = true;
+        break;
+      }
+    }
+
+    if (!should) {
+      result[key] = obj[key];
+    }
+
+   }
+  }
+  return result;
+}
+// 다중 반복문이라 좋은 코드는 아님
+
+console.log(omit({ a: 1, b: 2, c: 3 }, ["b"]));    // { a: 1, c: 3 }
+console.log(omit({ x: 10, y: 20 }, ["x", "y"]));   // {}
+console.log(omit({}, ["a"]));                      // {}
+console.log(omit({ a: 1, b: 2 }, []));             // { a: 1, b: 2 }
+
+// uh...?
