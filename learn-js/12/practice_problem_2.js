d09f3ec2e04cc553e3c6ef498f0da59b4d3c0e61
elecharu <elec_haru@naver.com>
2025-08-23 03:15:25 +0900

    20250822 study

--- learn-js/12/practice_problem_2.js
+++ learn-js/12/practice_problem_2.js
@@ -1,15 +1,21 @@
 // # 연습문제(고급)
 
 // ## 1. 객체의 중복 값 제거
-function removeDuplicateValues (obj1){
-    const result = {};
+function removeDuplicateValues (obj){
+    const values = Object.values(obj)
+    const freq = values.reduce((m,v) => {
+        m[v] = (m[v] || 0 ) + 1;
+        return m;
+    }, {})
     
-    const entries = Object.entries(obj1)
+    console.log(freq)
 
-    for ( const [k,v] of entries){
-        console.log(entries[k,v])
-    }
-
+    const entries = Object.entries(obj);
+    const out = entries.filter(([k,v]) => {
+        return freq[v] === 1;
+    })
+    const result = Object.fromEntries(out);
+    return result
 }
 
 const obj = { a: 1, b: 2, c: 1, d: 3 };
@@ -26,7 +32,19 @@ console.log(removeDuplicateValues(obj4)); // { n: 4 }
 
 // 2. 깊은 비교 (deepEqual)
 function deepEqual (obj1, obj2){
+    if(obj1 === obj2 ) return true;
+    if(obj1 === null || obj2 === null) return false;
+    if(typeof obj1 !== 'object' || typeof obj2 !== 'object') return false;
 
+    const aKeys = Object.keys(obj1)
+    const bKeys = Object.keys(obj2)
+
+    if(aKeys.length !== bKeys.length) return false
+    for(const key of aKeys){
+        if( !deepEqual(obj1[key], obj2[key])) return false;
+    }
+
+    return true;
 }
 
 
@@ -38,7 +56,18 @@ console.log(deepEqual({ a: 1 }, { a: 2 }));                 // false
 
 // 3. 깊은 동결 (deepFreeze)
 function deepFreeze (obj){
+    Object.freeze(obj)  // 얕은 객체의 동결
 
+    // 안에 존재하는 값들에 대한 동결을 재귀함수로 호출
+    for(const key in obj){
+        const value = obj[key];
+        if(value !== null && typeof value === 'object'){
+            if(!Object.freeze(value)){
+                deepFreeze(value)
+            }
+        }
+    }
+    return obj
 }
 
 const obj = { a: { b: 1 } };
